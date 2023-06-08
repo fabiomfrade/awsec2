d7ec4731b9d6f10c81ee8ec6198c70cbc2b8cf95
fabiomfrade <fabiomfrade@gmail.com>
2023-06-08 13:09:47 -0300

    Inserindo permissao de Lambda

--- eventbridge.tf
+++ eventbridge.tf
@@ -29,3 +29,23 @@ resource "aws_cloudwatch_event_target" "stop_ec2_target" {
   target_id = "StopEC2"
   arn       = aws_lambda_function.stopec2.arn
 }
+
+###################
+#   Permissions   #
+###################
+
+resource "aws_lambda_permission" "allow_startec2" {
+  statement_id  = "AllowExecutionFromEventBridgeStartEC2"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.startec2.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.start_ec2.arn
+}
+
+resource "aws_lambda_permission" "allow_stopec2" {
+  statement_id  = "AllowExecutionFromEventBridgeStopEC2"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.stopec2.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.stop_ec2.arn
+}
